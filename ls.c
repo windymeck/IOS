cf50b3810fd1d53153bf8aa2983f3e6222d9cac8
UnaiFernandez <fernandezunai027@gmail.com>
2020-03-05 18:25:59 +0100

    ls working

--- ls.c
+++ ls.c
@@ -1,30 +1,53 @@
 #include <stdio.h>
 #include <dirent.h>
-#include<stdlib.h>
-#include<fcntl.h>
-#include<sys/stat.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <unistd.h>
+
 int main(int argc, char *argv[])
 {
 	DIR *dirp;
 	struct dirent *dp;
+	struct stat fstats;
 	char *loc = NULL;
 	char *pointer = NULL;
+	char bar[1] = "/";
+
 	pointer = getenv("PWD");
 	if(argc == 1){
 		dirp = opendir((const char*)pointer);
 		loc = pointer;
-		printf("You are located in the %s", loc);
+		printf("You are located in %s\n", loc);
 		while((dp=readdir(dirp))!=NULL){
-			printf("%s \n",dp->d_name);
+			if(dp->d_name[0] != '.'){
+				stat(dp->d_name, &fstats);
+				//printf("Mode: %s%lo\n", dp->d_name, (unsigned long) fstats.st_mode);
+				if(fstats.st_mode & S_IFDIR)
+					printf("---->%s/ \n",dp->d_name);
+				else
+					printf("#%s\n", dp->d_name);
+			}
 		}
 		printf("\n");
 	}
 	else{
-		dirp = opendir(argv[1]);
-		loc = argv[1];
-		printf("You are located in the %s", loc);
+		strcat(pointer, bar);
+		strcat(pointer, argv[1]);
+		dirp = opendir((const char*)pointer);
+		loc = pointer;
+		printf("You are located in %s\n", loc);
 		while((dp=readdir(dirp))!=NULL){
-			printf("%s \n",dp->d_name);
+			if(dp->d_name[0] != '.'){
+				chdir(pointer);
+				stat(dp->d_name, &fstats);
+				//printf("Mode: %s%lo\n", dp->d_name, (unsigned long) fstats.st_mode);
+				if(fstats.st_mode & S_IFDIR)
+					printf("---->%s/ \n",dp->d_name);
+				else
+					printf("#%s\n", dp->d_name);
+			}
 		}
 		printf("\n");
 	}
